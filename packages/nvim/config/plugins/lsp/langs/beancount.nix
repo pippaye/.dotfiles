0efed041c8ff0a749481f4552a611c48aef7ba7f
ashenye <luren145@gmail.com>
2026-06-05 17:44:18 +0800

    x

--- packages/nvim/config/plugins/lsp/langs/beancount.nix
+++ packages/nvim/config/plugins/lsp/langs/beancount.nix
@@ -1,7 +1,30 @@
-{ lib, pkgs, ... }:
-{
+  { lib, pkgs, ... }:
+  {
+    filetype.extension = {
+      bean = "beancount";
+      beancount = "beancount";
+    };
 
-  plugins.lsp.servers = {
-    beancount.enable = true;
-  };
-}
+    plugins.lsp.servers.beancount = {
+      enable = true;
+
+      rootMarkers = [
+        "main.beancount"
+        "main.bean"
+        ".git"
+      ];
+
+      extraOptions = {
+        init_options = {
+          journal_file = "main.beancount";
+
+          diagnostic_flags = [ "!" ];
+
+          completion = {
+            fuzzy_match_accounts = true;
+          };
+
+        };
+      };
+    };
+  }
