011cc3bb4568ba53bfc6e8108c03e5520f5a1a84
ashenye <luren145@gmail.com>
2026-07-11 19:09:29 +0800

    workaround: nvim doc

--- packages/nvim/config/default.nix
+++ packages/nvim/config/default.nix
@@ -13,6 +13,10 @@
     byteCompileLua.enable = true;
   };
 
+  # FIXIT 生成doc依赖pandoc, 需要lua
+  # Nixvim's manpage build currently requires a pandoc build with Lua support.
+  enableMan = false;
+
   # Aliases
   viAlias = true;
   vimAlias = true;
