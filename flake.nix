4df2b4fb80e512ff4b2f45a8e90b460c83e4f523
ashenye <luren145@gmail.com>
2026-08-08 17:01:43 +0800

    workaround: 绕开unfree

--- flake.nix
+++ flake.nix
@@ -118,7 +118,13 @@
     (flake-utils.lib.eachSystem supportedSystems (
       system:
       let
-        pkgs = nixpkgs.legacyPackages."${system}";
+        pkgs = import nixpkgs {
+          inherit system;
+          config = {
+            allowUnfree = true;
+            allowBroken = true;
+          };
+        };
       in
       {
         formatter = pkgs.nixfmt;
